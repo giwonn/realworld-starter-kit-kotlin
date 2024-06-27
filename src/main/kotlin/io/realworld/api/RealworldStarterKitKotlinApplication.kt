21b6f1a076118ea72e124650567e0c09b274d03f
Allen <allendev@alphacode.co.kr>
2024-06-27 16:06:05 +0900

    project init

---
+++ src/main/kotlin/io/realworld/api/RealworldStarterKitKotlinApplication.kt
@@ -0,0 +1,11 @@
+package io.realworld.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealworldStarterKitKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealworldStarterKitKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/io/realworld/api/RealworldStarterKitKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.realworld.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldStarterKitKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
